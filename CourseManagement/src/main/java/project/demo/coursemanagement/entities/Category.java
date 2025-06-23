3de07295d7d21b808eba375939ac9f44367b7db7
QuanHTHE181258 <quanhthe181258@fpt.edu.vn>
2025-06-23 20:22:44 +0700

    Fixed Empty Category

--- CourseManagement/src/main/java/project/demo/coursemanagement/entities/Category.java
+++ CourseManagement/src/main/java/project/demo/coursemanagement/entities/Category.java
@@ -1,15 +1,38 @@
 package project.demo.coursemanagement.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Nationalized;
+
+import java.time.Instant;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CategoryID", nullable = false)
+    private Integer id;
 
-    //TODO [Reverse Engineering] generate columns from DB
+    @Size(max = 100)
+    @NotNull
+    @Nationalized
+    @Column(name = "Name", nullable = false, length = 100)
+    private String name;
+
+    @Nationalized
+    @Lob
+    @Column(name = "Description")
+    private String description;
+
+    @ColumnDefault("getdate()")
+    @Column(name = "CreatedAt")
+    private Instant createdAt;
+
 }
