febfbae5b2f4a9163082c0b80f05fa9b4f99e63d
HoangQLHE181552 <hoangqlhe181552@fpt.edu.vn>
2025-07-19 19:04:35 +0700

    thêm điều kiện cho xem lessons

--- CourseManagement/src/main/java/project/demo/coursemanagement/service/LessonPreviewService.java
+++ CourseManagement/src/main/java/project/demo/coursemanagement/service/LessonPreviewService.java
@@ -1,4 +1,20 @@
 package project.demo.coursemanagement.service;
 
+import project.demo.coursemanagement.dao.LessonPreviewDAO;
+import project.demo.coursemanagement.dao.impl.LessonPreviewDAOimp;
+import project.demo.coursemanagement.dto.LessonDTO;
+
+import java.math.BigDecimal;
+
 public class LessonPreviewService {
+    private final LessonPreviewDAO lessonDAO = new LessonPreviewDAOimp();
+
+    public LessonDTO getLesson(int lessonId) {
+        return lessonDAO.getLessonById(lessonId);
+    }
+
+    public BigDecimal getCoursePriceByLessonID(int lessonID) {
+        return lessonDAO.getCoursePriceByLessonID(lessonID);
+    }
+
 }
